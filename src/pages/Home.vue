3028076a34966e8f04d4ed7f4454190264abbc37
DecimTheJudge <paolo1piga@gmail.com>
2024-10-15 15:30:40 +0200

    add: card component

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -54,8 +54,10 @@
 <template>
     <h1 class="p-4 bg-blue-500 text-white text-center rounded-md sm:bg-red-500 md:bg-green-500">Benvenuto nella HomePage di BoolBnb!</h1>
 
-    <div class="container m-auto">
-        <div class="text-center">QUI CI FINIRA' LA BARRA DEI FILTRI</div>
+    <div class="container m-auto text">
+        <div class="text-center w-full bg-red-500 h-10">
+            QUI CI FINIRA' LA BARRA DEI FILTRI
+        </div>
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
             <!-- <div class="square bg-blue-500 rounded sm:bg-red-500 md:bg-green-500 lg:bg-blue-300" ></div> -->
             <div v-for="appartamento in appartamenti" :key="appartamento.title" class="square bg-blue-500 rounded-md sm:bg-red-500 md:bg-green-500 lg:bg-blue-300" >
