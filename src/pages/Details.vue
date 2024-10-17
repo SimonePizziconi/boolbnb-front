9e5329b9b51bef28ef2295342dac9affb888b7bc
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-10-17 18:32:20 +0200

    feat: add details page test

--- src/pages/Details.vue
+++ src/pages/Details.vue
@@ -1,22 +1,43 @@
 <script>
+    import axios from 'axios';
+    import { store } from '../store/store.js';
+
     export default {
-        name: 'Details'
+        name: 'Details',
+        data(){
+            return {
+                store,
+                slug: 'casa-in-montagna',
+                apartment: {}
+            }
+        },
+        methods: {
+            getApartment(){
+                axios.get(store.apiUrl + 'apartment/' + this.slug)
+                .then(res => {
+                    this.apartment = res.data.apartment;
+                    console.log(this.apartment);
+                })
+            }
+        },
+        mounted(){
+            this.getApartment();
+        }
     }
 </script>
 
 <template>
     <div class="container m-auto text">
-        <h1 class="">Titolo Appartamento</h1> <span>&hearts;</span>
+        <h1 class="">{{ apartment.title }}</h1> <span>&hearts;</span>
         <div class="flex">
             <div class="flex-1 w-8/10 h-96 mt-3">
-                <img class="rounded-md h-full w-full object-cover" src="https://panoramaliving.it/fileadmin/_processed_/0/4/csm_luxury-apartment-living-wohnk%C3%BCche_488071d7d8.jpg" alt="">
+                <img class="rounded-md h-full w-full object-cover" :src="apartment.image_path" :alt="apartment.title">
             </div>
             <div class="flex-1 w-2/10 h-96 mt-3 bg-gray-200 text-center"> 
-                <h6>Titolo Descrittivo Appartamento</h6>
-                <span>6 Stanze - 2 Letti - 2 Bagni - 20 M2 </span>
+                <h6>{{ apartment.title }}</h6>
+                <span>{{ apartment.rooms }} Stanze - {{ apartment.beds }} Letti - {{ apartment.bathrooms }} Bagni - {{ apartment.square_meters }} M2 </span>
             </div>
         </div>
-
         
     </div>
     
