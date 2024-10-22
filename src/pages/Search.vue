55e2bbce01e69c168115043a8ef1d9335f307394
BeneArp <benedetta.arpino@gmail.com>
2024-10-22 17:27:18 +0200

    feat: change search layout and responsive

--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -89,8 +89,8 @@ export default {
 <template>
     <div class="container m-auto mt-10 mb-10">
 
-        <div class="filters w-fit mx-auto p-8 bg-grey-100 border-2 border-bg-gray-100 bg-white shadow-lg rounded-3xl">
-          <form @submit.prevent="fetchSearchResults" class="grid grid-cols-5">
+        <div class="filters lg:w-fit mx-auto p-8 bg-grey-100 border-2 border-bg-gray-100 bg-white shadow-lg rounded-3xl">
+          <form @submit.prevent="fetchSearchResults" class="grid md:grid-cols-5 sm:grid-cols-1">
 
               <!-- selezione raggio -->
               <div>
@@ -98,7 +98,7 @@ export default {
                   <label for="radius">Distanza(km)</label>
                 </div>
                 <div class="text-center">
-                  <input class="w-1/3 rounded-lg focus:ring-2 focus:ring-inset focus:ring-accent border-none bg-gray-200" name="radius" id="radius" type="number" value="20" v-model="filterRadius" min="0">
+                  <input class="md:w-2/5 rounded-lg focus:ring-2 focus:ring-inset focus:ring-accent border-none bg-gray-200 text-center" name="radius" id="radius" type="number" value="20" v-model="filterRadius" min="0">
                 </div>
               </div>
 
@@ -108,7 +108,7 @@ export default {
                   <label for="rooms">Stanze:</label>
                 </div>
                 <div class="text-center">
-                  <input class="w-1/3 rounded-lg focus:ring-2 focus:ring-inset focus:ring-accent border-none bg-gray-200" name="rooms" id="rooms" type="number" value="1" v-model="rooms" min="1">
+                  <input class="md:w-2/5 rounded-lg focus:ring-2 focus:ring-inset focus:ring-accent border-none bg-gray-200 text-center" name="rooms" id="rooms" type="number" value="1" v-model="rooms" min="1">
                 </div>
               </div>
 
@@ -118,7 +118,7 @@ export default {
                   <label for="beds">Posti Letto:</label>
                 </div>
                 <div class="text-center">
-                  <input class="w-1/3 rounded-lg focus:ring-2 focus:ring-inset focus:ring-accent border-none bg-gray-200" name="beds" id="beds" type="number" value="1" v-model="beds" min="1">
+                  <input class="md:w-2/5 rounded-lg focus:ring-2 focus:ring-inset focus:ring-accent border-none bg-gray-200 text-center" name="beds" id="beds" type="number" value="1" v-model="beds" min="1">
                 </div>
               </div>
 
@@ -128,7 +128,7 @@ export default {
               </div>
 
               <!-- invio -->
-              <button class="auto-rows-max bg-secondary text-neutral font-bold w-fit h-fit p-2 ps-6 pe-6 rounded-lg mt-5 hover:bg-teal-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2         focus-visible:outline-secondary transition duration-300" type="submit">
+              <button class="auto-rows-max bg-secondary text-neutral font-bold w-fit h-fit p-2 ps-6 pe-6 rounded-lg mt-5 hover:bg-teal-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2         focus-visible:outline-secondary transition duration-300 mx-auto" type="submit">
                 {{ this.isLoading ? 'In corso...' : 'Filtra' }}
               </button>
           </form>
@@ -137,9 +137,9 @@ export default {
         <!-- Modal per la selezione dei servizi -->
         <div v-if="isModalOpen" class="fixed inset-0 z-50 flex items-center justify-center bg-primary bg-opacity-50">
           <div class="bg-white p-8 rounded-lg w-5/6">
-            <h2 class="text-xl mb-4">Seleziona i servizi</h2>
+            <h2 class="text-xl mb-4 font-bold">Seleziona i servizi</h2>
 
-            <div class="flex flex-col flex-wrap h-fit max-h-96">
+            <div class="flex flex-col lg:flex-wrap h-fit max-h-96 lg:overflow-hidden sm:overflow-auto">
               
               <!-- Lista servizi -->
               <div v-for="service in services" :key="service.id" class="flex items-center m-2">
@@ -151,8 +151,8 @@ export default {
 
             <!-- Bottoni per chiudere il modal -->
             <div class="mt-4 flex justify-center space-x-2">
-              <button @click="toggleModal" class="btn text-white px-4 py-2 rounded bg-accent">Applica</button>
-              <button @click="toggleModal" class="btn bg-gray-300 px-4 py-2 rounded ">Chiudi</button>
+              <button @click="toggleModal" class="btn text-white px-4 py-2 rounded bg-accent font-bold">Applica</button>
+              <button @click="toggleModal" class="btn bg-gray-300 px-4 py-2 rounded font-bold">Chiudi</button>
             </div>
 
           </div>
@@ -160,17 +160,23 @@ export default {
 
         <h3 class="mt-6">Numero Risultati: <strong>{{ this.count }}</strong></h3>
 
-        <div class="grid grid-cols-4">
+        <div class="grid lg:grid-cols-4 pb-20 md:grid-cols-2 sm:grid-cols-1 gap-4">
 
           <div v-for="apartment in apartments" :key="apartment.id">
-            <router-link :to="{name:'details', params:{slug: apartment.slug}}" class="card gap-1 mt-5 flex flex-col justify-center items-center text-center relative">
+            <router-link :to="{name:'details', params:{slug: apartment.slug}}" class="card mt-5 flex flex-col justify-center items-center relative shadow-lg">
               <img 
                 :src="apartment.image_path" 
                 :alt="apartment.title" 
-                class="w-full h-full rounded object-cover"
+                class="w-full h-full rounded-t object-cover"
               >
               <!-- Dettagli dell'appartamento -->
-              <h6 class="text-center mt-2">{{ apartment.title }}</h6>
+              <div class="text-left w-full bg-background rounded-b ps-4 pe-4 text-center truncate">
+                <h6 class="font-bold text-secondary mt-3 mb-4 sm:text-xl lg:text-lg truncate">{{ apartment.title }}</h6>
+
+                <span class="lg:text-sm sm:text-md pb-2 pt-2 mt-4 border-b-2 border-t-2 border-accent me-2 truncate"><i class="fa-solid fa-location-dot me-2 text-accent"></i>{{ apartment.address }}</span>
+                <span class="lg:text-sm sm:text-md pb-2 pt-2 mt-2 mb-2 block truncate"><i class="fa-solid fa-house-chimney text-secondary"></i> Stanze - {{ apartment.rooms }}  <i class="fa-solid fa-pen-ruler ms-3 text-secondary"></i> Metri quadrati - {{ apartment.square_meters }}</span>
+              </div>
+              
             </router-link>
           </div>
 
@@ -182,8 +188,7 @@ export default {
 <style scoped lang="scss">
 
 .card{
-  width: 305px;
-  margin-right: 20px;
+  width: calc(100%);
   aspect-ratio: 1/1
 }
 
