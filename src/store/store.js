b825462b2a00b4c5b0acbd4ab1ec153179d8d19e
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-10-23 11:30:55 +0200

    feat: update url with query & filters

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,13 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-    apiUrl: 'http://localhost:8000/api/'
+    apiUrl: 'http://localhost:8000/api/',
+    searchQuery: '',
+    lat: '',
+    lng: '',
+    radius: 20,
+    rooms: 1,
+    beds: 1,
+    services: [],
+    selectedServices: []
 });
